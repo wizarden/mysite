a161935ab76226a074de552203ec7f35abf58b9c
asrock <asrock@DENIS>
2020-08-02 23:35:37 +0300

    carusel

--- base/admin.py
+++ base/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 
-class ProductImageInline(admin.StackedInline):
+class ProductImageInline(admin.TabularInline): #TabularInline StackedInline
     model = ProductImage
     extra = 0
 
