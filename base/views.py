a3e548bb23a0ba5dc9e10f831091a9f6f6e6e3df
asrock <asrock@DENIS>
2020-08-12 23:18:54 +0300

    get post

--- base/views.py
+++ base/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from .models import Product, ProductCategory, Slider
+from django.http import HttpResponse
 # Create your views here.
+
 
 
 from django.http import HttpResponse
 
+
+def index2(request):
+    my="my22"
+    print(2)
+    
+    return render(request,'base/index2.html', locals())
+
+def index3(request):
+    print(3)
+ #   print(request.GET)
+ #   print(request.POST)
+    my="my3"
+    return render(request,'base/index2.html', locals())
+
+
+
+#return HttpResponse("123")
 
 def index(request):
     product_list = Product.objects.order_by('name')
@@ -15,5 +34,11 @@ def index(request):
     return render(request,'base/index.html', locals())
 
 
-
+def category(request):
+    
+    print(request.method)
+    print(request.GET)
+    print(request.POST)
+    
+    return render(request,'base/category.html', locals())
 
